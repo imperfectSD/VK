ad5c5a78bb602599006cd010672c346e62db8660
Angelina Dadiverina <imperfect.sparda@gmail.com>
2021-03-08 19:39:30 +0300

    post manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended, String fields) {
+        return null;
+    }
+
+    public Post[] delete (int ownerId, int postId) {
+        return null;
+    }
+}
